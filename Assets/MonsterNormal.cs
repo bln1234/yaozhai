ec92ab546fe463a449471c4abec6f4bbec483cc7
bln <1974568595@qq.com>
2024-11-15 20:01:23 +0800

    创建史莱姆

--- Assets/MonsterNormal.cs
+++ Assets/MonsterNormal.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class MonsterNormal : MonoBehaviour
 {
-    private Collider MonsterCollider; // monster的碰撞器
-    private Material originalMaterial; // 用于保存原本的材质
-    private Color originalColor; // 用于保存原本的颜色
+    public GameObject fox;
+    public Material[] materials; // 怪物身上所有材质
     public Color hitColor = Color.red; // 碰撞后变成的颜色
+
+    public float MaxDistance;
+    private float JumpCooldown = 2f; // 跳跃攻击冷却时间
+    private float lastJumpTime; //上次跳跃攻击时间
+    private float VerticalV; // 垂直方向速度
+    private float VerticalA; // 垂直方向加速度
+    private float HorizontalV; // 水平方向速度
+    private float HorizontalA; // 水平方向加速度
+
+    private bool isJump;
+
+    private Animator animator;
+    private Collider MonsterCollider; // monster的碰撞器
+    private Color[] originalColor; // 用于保存原本的颜色
+    private Vector3 move = Vector3.zero; // 移动方向
+
     // Start is called before the first frame update
     void Start()
     {
-        // 获取原始材质和颜色
-        Renderer renderer = GetComponent<Renderer>();
-        originalMaterial = renderer.material;
-        originalColor = originalMaterial.color;
+        isJump = false; // 初始不跳跃攻击
+        animator = GetComponent<Animator>();
+        originalColor = new Color[materials.Length]; // 原始颜色
+        for ( int i = 0; i < materials.Length;  i++ )
+        {
+            originalColor[i] = materials[i].color;
+        }
         MonsterCollider = transform.GetComponent<Collider>(); // 怪物的碰撞器
         MonsterCollider.enabled = true;
     }
@@ -23,6 +41,54 @@ public class MonsterNormal : MonoBehaviour
     void Update()
     {
         
+        // 怪物不移动
+        if (fox.transform.position.z - transform.position.z > MaxDistance
+            || transform.position.z - fox.transform.position.z > MaxDistance)
+        {
+            animator.SetBool("isWalk", false);
+        }
+        else
+        {
+            // 跳跃攻击
+            if (Time.time >= lastJumpTime + JumpCooldown)
+            {
+                animator.SetTrigger("isJump");
+                isJump = true;
+                VerticalV = 1f;
+                VerticalA = -2f;
+                if (fox.transform.position.z - transform.position.z > 0)
+                {
+                    move.z = 0.01f;
+                    HorizontalV = transform.position.z - fox.transform.position.z;
+                }
+                else
+                {
+                    move.z = -0.01f;
+                    HorizontalV = fox.transform.position.z - transform.position.z;
+                }
+                transform.rotation = Quaternion.LookRotation(move);
+                move = new Vector3(0, VerticalV, HorizontalV);
+                transform.position += move * Time.deltaTime; // 更新位置
+                lastJumpTime = Time.time;
+                isJump = false;
+            }
+            // 怪物移动
+            if(!isJump)
+            {
+                animator.SetBool("isWalk", true);
+                if (fox.transform.position.z - transform.position.z > 0)
+                {
+                    move.z = 0.01f;
+                }
+                else
+                {
+                    move.z = -0.01f;
+                }
+                transform.rotation = Quaternion.LookRotation(move);
+                transform.position += move;
+            }
+            
+        }
     }
     void OnTriggerEnter(Collider collision)
     {
@@ -36,7 +102,10 @@ public class MonsterNormal : MonoBehaviour
     }
     private IEnumerator BeAttacked()
     {
-        originalMaterial.color = hitColor;
+        for( int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = hitColor;
+        }
         Vector3 originalPosition = transform.position;
         for (int i = 0; i < 5; i++) 
         {
@@ -46,6 +115,9 @@ public class MonsterNormal : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
         transform.position = originalPosition;
-        originalMaterial.color = originalColor;
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColor[i];
+        }
     }
 }
